c4e3dd69e556e2077394ea4cfe2bf8b66254e16b
Biamah <beatriz.souza.costa@outlook.com>
2025-04-16 17:29:20 -0300

    feat: add header component and update styles

---
+++ src/app/components/header.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { FaLinkedin, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { Button } from "./Button";
+import { Social } from "./social";
+
+export const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-transparent py-6">
+      <div className="container mx-auto px-6 flex justify-end items-center">
+        <div className="absolute left-1/2 transform -translate-x-1/2">
+          <img src="/logo.png" alt="Logo" className="h-12" />
+        </div>
+
+        <div className="flex gap-4">
+          <Button
+            href="https://wa.me/5185424053"
+            text="Contato"
+            icon={<FaWhatsapp />}
+          />
+          <Social
+            icon={<FaLinkedin />}
+            link="https://www.linkedin.com"
+            label="linkedin"
+          />
+          <Social
+            icon={<FaInstagram />}
+            link="https://www.instagram.com/nekkomassage"
+            label="Instagram"
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
