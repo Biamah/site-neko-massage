dd959e4bc604b494e8a4731d1c4dc7f3bbc67840
Biamah <beatriz.souza.costa@outlook.com>
2025-06-21 17:51:22 -0300

    feat: create component aboutUs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Header } from "./components/header";
 import BannerCarousel from "./components/bannerCarousel";
 import { slidesItems } from "./constants/slideItems";
+import AboutUs from "./components/aboutUs";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <BannerCarousel slides={slidesItems} options={{ loop: true }} />
+      <AboutUs />
     </div>
   );
 }
